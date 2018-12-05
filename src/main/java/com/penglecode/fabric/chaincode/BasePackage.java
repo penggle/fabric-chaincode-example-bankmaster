6408eb4810ded6cb085d1bf334c9f475b0031cae
pengp <pengp@certusnet.com.cn>
2018-12-05 09:20:17 +0800

    first commit

---
+++ src/main/java/com/penglecode/fabric/chaincode/BasePackage.java
@@ -0,0 +1,5 @@
+package com.penglecode.fabric.chaincode;
+
+public class BasePackage {
+
+}
---
+++ src/main/java/com/penglecode/fabric/chaincode/bankmaster/boot/BankMasterApplication.java
@@ -0,0 +1,33 @@
+package com.penglecode.fabric.chaincode.bankmaster.boot;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.penglecode.fabric.chaincode.BasePackage;
+import com.penglecode.fabric.chaincode.bankmaster.chaincode.BankMasterChaincode;
+
+@SpringBootApplication(scanBasePackageClasses=BasePackage.class)
+public class BankMasterApplication implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(BankMasterApplication.class);
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(BankMasterApplication.class)
+			.web(WebApplicationType.NONE)
+			.run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info(">>> Starting chaincode, args = {}", Arrays.toString(args));
+		new BankMasterChaincode().start(args);
+		//LockSupport.park();
+	}
+
+}
